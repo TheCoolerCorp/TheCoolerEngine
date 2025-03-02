97ef284f31cd483b3a2bdc5f852152219bf83531
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2025-03-02 23:32:03 +0100

    little modification in vec2 unit test

--- Engine/Tests/UnitTests/src/vec2UT.cpp
+++ Engine/Tests/UnitTests/src/vec2UT.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include "vec2UT.h"
 
+using namespace TheCoolerMath;
+
 namespace vec2UT
 {
 	/*------------------------------------------CONSTRUCTORS-----------------------------------------------------*/ 
 	TEST(Vector2, DefaultConstructor)
 	{
-		TheCoolerMath::vec2 v;
+		vec2 v;
 
-		EXPECT_EQ(v.x, 0.f);
-		EXPECT_EQ(v.y, 0.f);
+		EXPECT_FLOAT_EQ(v.x, 0.f);
+		EXPECT_FLOAT_EQ(v.y, 0.f);
 	}
 
 	TEST(Vector2, BasicConstructor)
@@ -17,20 +19,20 @@ namespace vec2UT
 		float t_value1 = 0.f;
 		float t_value2 = 1.f;
 
-		TheCoolerMath::vec2 v = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v = vec2(t_value1, t_value2);
 
-		EXPECT_EQ(v.x, t_value1);
-		EXPECT_EQ(v.y, t_value2);
+		EXPECT_FLOAT_EQ(v.x, t_value1);
+		EXPECT_FLOAT_EQ(v.y, t_value2);
 	}
 
 	TEST(Vector2, SingleValueConstructor)
 	{
 		float t_value = 1.f;
 
-		TheCoolerMath::vec2 v = TheCoolerMath::vec2(t_value);
+		vec2 v = vec2(t_value);
 
-		EXPECT_EQ(v.x, t_value);
-		EXPECT_EQ(v.y, t_value);
+		EXPECT_FLOAT_EQ(v.x, t_value);
+		EXPECT_FLOAT_EQ(v.y, t_value);
 	}
 
 	/*------------------------------------------Functions-----------------------------------------------------*/
@@ -39,11 +41,11 @@ namespace vec2UT
 		float t_value1 = 3.f;
 		float t_value2 = 5.f;
 
-		TheCoolerMath::vec2 v = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v = vec2(t_value1, t_value2);
 		v.Opposite();
 
-		EXPECT_EQ(v.x, -t_value1);
-		EXPECT_EQ(v.y, -t_value2);
+		EXPECT_FLOAT_EQ(v.x, -t_value1);
+		EXPECT_FLOAT_EQ(v.y, -t_value2);
 	}
 
 	TEST(Vector2, Invert)
@@ -51,11 +53,11 @@ namespace vec2UT
 		float t_value1 = 3.f;
 		float t_value2 = 5.f;
 
-		TheCoolerMath::vec2 v = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v = vec2(t_value1, t_value2);
 		v.Invert();
 
-		EXPECT_EQ(v.x, 1.f / t_value1);
-		EXPECT_EQ(v.y, 1.f / t_value2);
+		EXPECT_FLOAT_EQ(v.x, 1.f / t_value1);
+		EXPECT_FLOAT_EQ(v.y, 1.f / t_value2);
 	}
 
 	TEST(Vector2, AddNumber)
@@ -64,11 +66,11 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float add = 2.f;
 
-		TheCoolerMath::vec2 v = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v = vec2(t_value1, t_value2);
 		v.AddNumber(add);
 		
-		EXPECT_EQ(v.x, t_value1 + add);
-		EXPECT_EQ(v.y, t_value2 + add);
+		EXPECT_FLOAT_EQ(v.x, t_value1 + add);
+		EXPECT_FLOAT_EQ(v.y, t_value2 + add);
 	}
 
 	TEST(Vector2, MultiplyNumber)
@@ -77,11 +79,11 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float multiply = 2.f;
 
-		TheCoolerMath::vec2 v = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v = vec2(t_value1, t_value2);
 		v.MultiplyNumber(multiply);
 
-		EXPECT_EQ(v.x, t_value1 * multiply);
-		EXPECT_EQ(v.y, t_value2 * multiply);
+		EXPECT_FLOAT_EQ(v.x, t_value1 * multiply);
+		EXPECT_FLOAT_EQ(v.y, t_value2 * multiply);
 	}
 
 	TEST(Vector2, Add)
@@ -92,16 +94,16 @@ namespace vec2UT
 		float t_value3 = 4.f;
 		float t_value4 = 8.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
-		TheCoolerMath::vec2 v3 = TheCoolerMath::vec2::Add(v1, v2);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
+		vec2 v3 = vec2::Add(v1, v2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 		glm::vec2 v3g = v1g + v2g;
 
-		EXPECT_EQ(v3.x, v3g.x);
-		EXPECT_EQ(v3.y, v3g.y);
+		EXPECT_FLOAT_EQ(v3.x, v3g.x);
+		EXPECT_FLOAT_EQ(v3.y, v3g.y);
 	}
 
 	TEST(Vector2, Multiply)
@@ -112,16 +114,16 @@ namespace vec2UT
 		float t_value3 = 4.f;
 		float t_value4 = 8.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
-		TheCoolerMath::vec2 v3 = TheCoolerMath::vec2::Multiply(v1, v2);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
+		vec2 v3 = vec2::Multiply(v1, v2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 		glm::vec2 v3g = v1g * v2g;
 
-		EXPECT_EQ(v3.x, v3g.x);
-		EXPECT_EQ(v3.y, v3g.y);
+		EXPECT_FLOAT_EQ(v3.x, v3g.x);
+		EXPECT_FLOAT_EQ(v3.y, v3g.y);
 	}
 
 	TEST(Vector2, MidPoint)
@@ -132,14 +134,14 @@ namespace vec2UT
 		float t_value3 = 4.f;
 		float t_value4 = 8.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
-		TheCoolerMath::vec2 v3 = TheCoolerMath::vec2::MidPoint(v1, v2);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
+		vec2 v3 = vec2::MidPoint(v1, v2);
 
-		TheCoolerMath::vec2 v4 = (v1 + v2) / 2;
+		vec2 v4 = (v1 + v2) / 2;
 
-		EXPECT_EQ(v3.x, v4.x);
-		EXPECT_EQ(v3.y, v4.y);
+		EXPECT_FLOAT_EQ(v3.x, v4.x);
+		EXPECT_FLOAT_EQ(v3.y, v4.y);
 	}
 
 	TEST(Vector2, Distance)
@@ -150,17 +152,17 @@ namespace vec2UT
 		float t_value3 = 4.f;
 		float t_value4 = 8.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
-		float distance = TheCoolerMath::vec2::Distance(v1, v2);
+		float distance = vec2::Distance(v1, v2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 
 		float gDistance = glm::distance(v1g, v2g);
 
-		EXPECT_EQ(distance, gDistance);
+		EXPECT_FLOAT_EQ(distance, gDistance);
 	}
 
 	TEST(Vector2, SquaredNorm)
@@ -168,15 +170,15 @@ namespace vec2UT
 		float t_value1 = 3.f;
 		float t_value2 = 5.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
 
-		float squaredNorm = TheCoolerMath::vec2::SquaredNorm(v1);
+		float squaredNorm = vec2::SquaredNorm(v1);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 
 		float gSquaredNorm = glm::dot(v1g, v1g);
 
-		EXPECT_EQ(squaredNorm, gSquaredNorm);
+		EXPECT_FLOAT_EQ(squaredNorm, gSquaredNorm);
 	}
 
 	TEST(Vector2, Norm)
@@ -184,15 +186,15 @@ namespace vec2UT
 		float t_value1 = 3.f;
 		float t_value2 = 5.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
 
-		float Norm = TheCoolerMath::vec2::Norm(v1);
+		float Norm = vec2::Norm(v1);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 
 		float gNorm = glm::length(v1g);
 
-		EXPECT_EQ(Norm, gNorm);
+		EXPECT_FLOAT_EQ(Norm, gNorm);
 	}
 
 	TEST(Vector2, Normalize)
@@ -200,16 +202,16 @@ namespace vec2UT
 		float t_value1 = 3.f;
 		float t_value2 = 5.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
 
-		TheCoolerMath::vec2 nV1 = TheCoolerMath::vec2::Normalize(v1);
+		vec2 nV1 = vec2::Normalize(v1);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 
 		glm::vec2 nV1g = glm::normalize(v1g);
 
-		EXPECT_EQ(nV1.x, nV1g.x);
-		EXPECT_EQ(nV1.y, nV1g.y);
+		EXPECT_FLOAT_EQ(nV1.x, nV1g.x);
+		EXPECT_FLOAT_EQ(nV1.y, nV1g.y);
 	}
 
 	TEST(Vector2, DotProduct)
@@ -220,17 +222,17 @@ namespace vec2UT
 		float t_value3 = 4.f;
 		float t_value4 = 8.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
-		float dotProduct = TheCoolerMath::vec2::DotProduct(v1, v2);
+		float dotProduct = vec2::DotProduct(v1, v2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 
 		float gDotProduct = glm::dot(v1g, v2g);
 
-		EXPECT_EQ(dotProduct, gDotProduct);
+		EXPECT_FLOAT_EQ(dotProduct, gDotProduct);
 	}
 
 	TEST(Vector2, CrossProduct)
@@ -241,14 +243,14 @@ namespace vec2UT
 		float t_value3 = 4.f;
 		float t_value4 = 8.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
-		float crossProduct = TheCoolerMath::vec2::CrossProduct(v1, v2);
+		float crossProduct = vec2::CrossProduct(v1, v2);
 
 		float otherCrossProduct = (v1.x * v2.y) - (v2.x * v1.y);
 
-		EXPECT_EQ(crossProduct, otherCrossProduct);
+		EXPECT_FLOAT_EQ(crossProduct, otherCrossProduct);
 	}
 
 	TEST(Vector2, GetAngle)
@@ -259,10 +261,10 @@ namespace vec2UT
 		float t_value3 = 0.995f;
 		float t_value4 = 0.0998f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
-		float angle = TheCoolerMath::vec2::GetAngle(v1, v2);
+		float angle = vec2::GetAngle(v1, v2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
@@ -278,10 +280,10 @@ namespace vec2UT
 		float t_value1 = 3.f;
 		float t_value2 = 5.f;
 
-		TheCoolerMath::vec2 v = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v = vec2(t_value1, t_value2);
 
-		EXPECT_EQ(v[0], t_value1);
-		EXPECT_EQ(v[1], t_value2);
+		EXPECT_FLOAT_EQ(v[0], t_value1);
+		EXPECT_FLOAT_EQ(v[1], t_value2);
 	}
 
 	TEST(Vector2, OperatorEqual)
@@ -289,8 +291,8 @@ namespace vec2UT
 		float t_value1 = 3.f;
 		float t_value2 = 5.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value1, t_value2);
 
 		bool equal = v1 == v2;
 
@@ -299,7 +301,7 @@ namespace vec2UT
 
 		bool gEqual = v1g == v2g;
 
-		EXPECT_EQ(equal, gEqual);
+		EXPECT_FLOAT_EQ(equal, gEqual);
 	}
 
 	TEST(Vector2, OperatorAddNumber)
@@ -308,20 +310,20 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float add = 2.f;
 
-		TheCoolerMath::vec2 vO1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 vO1 = vec2(t_value1, t_value2);
 		vO1 = vO1 + add;
 
-		TheCoolerMath::vec2 vO2 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 vO2 = vec2(t_value1, t_value2);
 		vO2 = add + vO2;
 
 		glm::vec2 vO1g = glm::vec2(t_value1, t_value2);
 		vO1g = vO1g + add;
 
-		EXPECT_EQ(vO1g.x, vO1.x);
-		EXPECT_EQ(vO1g.y, vO1.y);
+		EXPECT_FLOAT_EQ(vO1g.x, vO1.x);
+		EXPECT_FLOAT_EQ(vO1g.y, vO1.y);
 
-		EXPECT_EQ(vO1g.x, vO2.x);
-		EXPECT_EQ(vO1g.y, vO2.y);
+		EXPECT_FLOAT_EQ(vO1g.x, vO2.x);
+		EXPECT_FLOAT_EQ(vO1g.y, vO2.y);
 	}
 
 	TEST(Vector2, OperatorSubtractNumber)
@@ -330,14 +332,14 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float sub = 2.f;
 
-		TheCoolerMath::vec2 vO1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 vO1 = vec2(t_value1, t_value2);
 		vO1 = vO1 - sub;
 
 		glm::vec2 vO1g = glm::vec2(t_value1, t_value2);
 		vO1g = vO1g - sub;
 
-		EXPECT_EQ(vO1g.x, vO1.x);
-		EXPECT_EQ(vO1g.y, vO1.y);
+		EXPECT_FLOAT_EQ(vO1g.x, vO1.x);
+		EXPECT_FLOAT_EQ(vO1g.y, vO1.y);
 	}
 	
 	TEST(Vector2, OperatorMultiplyNumber)
@@ -346,15 +348,15 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float multiply = 2.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 
-		TheCoolerMath::vec2 v2 = v1 * multiply;
+		vec2 v2 = v1 * multiply;
 		glm::vec2 v2g = v1g * multiply;
 
-		EXPECT_EQ(v2.x, v2g.x);
-		EXPECT_EQ(v2.y, v2g.y);
+		EXPECT_FLOAT_EQ(v2.x, v2g.x);
+		EXPECT_FLOAT_EQ(v2.y, v2g.y);
 	}
 
 	TEST(Vector2, OperatorDivideNumber)
@@ -363,14 +365,14 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float divide = 2.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 
-		TheCoolerMath::vec2 v2 = v1 / divide;
+		vec2 v2 = v1 / divide;
 		glm::vec2 v2g = v1g / divide;
 
-		EXPECT_EQ(v2.x, v2g.x);
-		EXPECT_EQ(v2.y, v2g.y);
+		EXPECT_FLOAT_EQ(v2.x, v2g.x);
+		EXPECT_FLOAT_EQ(v2.y, v2g.y);
 	}
 
 	TEST(Vector2, OperatorAdd)
@@ -380,17 +382,17 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 
-		TheCoolerMath::vec2 v3 = v1 + v2;
+		vec2 v3 = v1 + v2;
 		glm::vec2 v3g = v1g + v2g;
 
-		EXPECT_EQ(v3.x, v3g.x);
-		EXPECT_EQ(v3.y, v3g.y);
+		EXPECT_FLOAT_EQ(v3.x, v3g.x);
+		EXPECT_FLOAT_EQ(v3.y, v3g.y);
 	}
 
 	TEST(Vector2, OperatorSubtract)
@@ -401,17 +403,17 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 
-		TheCoolerMath::vec2 v3 = v1 - v2;
+		vec2 v3 = v1 - v2;
 		glm::vec2 v3g = v1g - v2g;
 
-		EXPECT_EQ(v3.x, v3g.x);
-		EXPECT_EQ(v3.y, v3g.y);
+		EXPECT_FLOAT_EQ(v3.x, v3g.x);
+		EXPECT_FLOAT_EQ(v3.y, v3g.y);
 	}
 
 	TEST(Vector2, OperatorMultiply)
@@ -422,17 +424,17 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 
-		TheCoolerMath::vec2 v3 = v1 * v2;
+		vec2 v3 = v1 * v2;
 		glm::vec2 v3g = v1g * v2g;
 
-		EXPECT_EQ(v3.x, v3g.x);
-		EXPECT_EQ(v3.y, v3g.y);
+		EXPECT_FLOAT_EQ(v3.x, v3g.x);
+		EXPECT_FLOAT_EQ(v3.y, v3g.y);
 	}
 
 	TEST(Vector2, OperatorDivide)
@@ -443,17 +445,17 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
-		TheCoolerMath::vec2 v2 = TheCoolerMath::vec2(t_value3, t_value4);
+		vec2 v1 = vec2(t_value1, t_value2);
+		vec2 v2 = vec2(t_value3, t_value4);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 		glm::vec2 v2g = glm::vec2(t_value3, t_value4);
 
-		TheCoolerMath::vec2 v3 = v1 / v2;
+		vec2 v3 = v1 / v2;
 		glm::vec2 v3g = v1g / v2g;
 
-		EXPECT_EQ(v3.x, v3g.x);
-		EXPECT_EQ(v3.y, v3g.y);
+		EXPECT_FLOAT_EQ(v3.x, v3g.x);
+		EXPECT_FLOAT_EQ(v3.y, v3g.y);
 	}
 
 	TEST(Vector2, OperatorEqualAdd)
@@ -464,8 +466,8 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1(t_value1, t_value2);
-		TheCoolerMath::vec2 v2(t_value3, t_value4);
+		vec2 v1(t_value1, t_value2);
+		vec2 v2(t_value3, t_value4);
 
 		glm::vec2 v1g(t_value1, t_value2);
 		glm::vec2 v2g(t_value3, t_value4);
@@ -473,8 +475,8 @@ namespace vec2UT
 		v1 += v2;
 		v1g += v2g;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 	}
 
 	TEST(Vector2, OperatorEqualSubtract)
@@ -485,8 +487,8 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1(t_value1, t_value2);
-		TheCoolerMath::vec2 v2(t_value3, t_value4);
+		vec2 v1(t_value1, t_value2);
+		vec2 v2(t_value3, t_value4);
 
 		glm::vec2 v1g(t_value1, t_value2);
 		glm::vec2 v2g(t_value3, t_value4);
@@ -494,8 +496,8 @@ namespace vec2UT
 		v1 -= v2;
 		v1g -= v2g;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 	}
 
 	TEST(Vector2, OperatorEqualMultiply)
@@ -506,8 +508,8 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1(t_value1, t_value2);
-		TheCoolerMath::vec2 v2(t_value3, t_value4);
+		vec2 v1(t_value1, t_value2);
+		vec2 v2(t_value3, t_value4);
 
 		glm::vec2 v1g(t_value1, t_value2);
 		glm::vec2 v2g(t_value3, t_value4);
@@ -515,8 +517,8 @@ namespace vec2UT
 		v1 *= v2;
 		v1g *= v2g;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 	}
 
 	TEST(Vector2, OperatorEqualDivide)
@@ -527,8 +529,8 @@ namespace vec2UT
 		float t_value3 = 2.f;
 		float t_value4 = 4.f;
 
-		TheCoolerMath::vec2 v1(t_value1, t_value2);
-		TheCoolerMath::vec2 v2(t_value3, t_value4);
+		vec2 v1(t_value1, t_value2);
+		vec2 v2(t_value3, t_value4);
 
 		glm::vec2 v1g(t_value1, t_value2);
 		glm::vec2 v2g(t_value3, t_value4);
@@ -536,8 +538,8 @@ namespace vec2UT
 		v1 /= v2;
 		v1g /= v2g;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 	}
 
 	TEST(Vector2, OperatorEqualAddNumber)
@@ -546,15 +548,15 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float add = 2.f;
 
-		TheCoolerMath::vec2 v1(t_value1, t_value2);
+		vec2 v1(t_value1, t_value2);
 
 		glm::vec2 v1g(t_value1, t_value2);
 
 		v1 += add;
 		v1g += add;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 
 	}
 
@@ -564,15 +566,15 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float sub = 2.f;
 
-		TheCoolerMath::vec2 v1(t_value1, t_value2);
+		vec2 v1(t_value1, t_value2);
 
 		glm::vec2 v1g(t_value1, t_value2);
 
 		v1 -= sub;
 		v1g -= sub;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 	}
 
 	TEST(Vector2, OperatorEqualDivideNumber)
@@ -581,15 +583,15 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float divide = 2.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 
 		v1 /= divide;
 		v1g /= divide;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 	}
 
 	TEST(Vector2, OperatorEqualMutliplyNumber)
@@ -598,14 +600,14 @@ namespace vec2UT
 		float t_value2 = 5.f;
 		float multiply = 2.f;
 
-		TheCoolerMath::vec2 v1 = TheCoolerMath::vec2(t_value1, t_value2);
+		vec2 v1 = vec2(t_value1, t_value2);
 
 		glm::vec2 v1g = glm::vec2(t_value1, t_value2);
 
 		v1 *= multiply;
 		v1g *= multiply;
 
-		EXPECT_EQ(v1.x, v1g.x);
-		EXPECT_EQ(v1.y, v1g.y);
+		EXPECT_FLOAT_EQ(v1.x, v1g.x);
+		EXPECT_FLOAT_EQ(v1.y, v1g.y);
 	}
 }
