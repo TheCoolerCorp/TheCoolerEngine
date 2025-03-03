01c7e686997a425ba0e67d091014615391803bff
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2025-03-03 12:02:32 +0100

    Unit tests for math utils done

---
+++ Engine/Tests/UnitTests/include/MathUtilsUT.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <gtest/gtest.h>
+
+#include "Math/TheCoolerMath.h"
+
+namespace MathUtilsUT
+{
+
+}
---
+++ Engine/Tests/UnitTests/src/MathUtilsUT.cpp
@@ -0,0 +1,29 @@
+#include "MathUtilsUT.h"
+
+using namespace TheCoolerMath;
+
+namespace MathUtilsUT
+{
+	TEST(MathUtils, Lerp)
+	{
+		float nb1 = 0.f;
+		float nb2 = 1.f;
+		float t = 0.5f;
+		float result = Lerp(nb1, nb2, t);
+		EXPECT_FLOAT_EQ(result, nb1 + t * (nb2 - nb1));
+	}
+
+	TEST(MathUtils, ToRadians)
+	{
+		float degrees = 180.f;
+		float radians = ToRadians(degrees);
+		EXPECT_FLOAT_EQ(radians, PI);
+	}
+
+	TEST(MathUtils, ToDegrees)
+	{
+		float radians = PI;
+		float degrees = ToDegrees(radians);
+		EXPECT_FLOAT_EQ(degrees, 180.f);
+	}
+}
