6a17f15d0c18b46e0fabf9d7576d1749ce6f8cbb
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2025-03-13 09:35:28 +0100

    Add cast on commandpool

--- Engine/Code/include/Core/Interfaces/ICommandPool.h
+++ Engine/Code/include/Core/Interfaces/ICommandPool.h
@@ -2,6 +2,7 @@
 #define ICOMMANDPOOL_H
 
 #include "EngineExport.h"
+#include "Core/Logger/Logger.h"
 
 namespace Engine
 {
@@ -24,6 +25,7 @@ namespace Engine
 				virtual ~ICommandPool() = default;
 				virtual void Create(IPhysicalDevice* a_physicalDevice, ISurface* a_surface, ILogicalDevice* a_logicalDevice) = 0;
 				virtual void Destroy(ILogicalDevice* a_logicalDevice) = 0;
+				virtual GraphicsAPI::VulkanCommandPool* CastVulkan() { LOG_ERROR("Try to return wrong cast type : VulkanCommandPool!"); return nullptr; }
 			};
 		}
 	}
