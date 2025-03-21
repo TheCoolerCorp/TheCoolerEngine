fe156279635bfb3ccd8f386d766d9cc8bb7bf218
e.rose <e.rose@student.isartdigital.com>
2025-03-21 16:29:05 +0100

    Start Input action

--- Engine/Code/include/Core/Window/IInputHandler.h
+++ Engine/Code/include/Core/Window/IInputHandler.h
@@ -20,16 +20,16 @@ namespace Engine
 
 				ENGINE_API virtual GLInputHandler* CastGLFW() { LOG_ERROR("Can't Cast!");return nullptr; }
 
-				ENGINE_API static void KeyCallBack(IWindow* a_window, int a_key, int a_scancode, int a_action, int a_mods) {}
-				ENGINE_API static void MouseCallBack(IWindow* a_window, int button, int a_action, int a_mods) {}
+				ENGINE_API virtual bool IsKeyDown(int a_key) = 0;
+				ENGINE_API virtual bool IsKeyPressed(int a_key) = 0;
+				ENGINE_API virtual bool IsKeyReleased(int a_key) = 0;
 
-				ENGINE_API static bool IsKeyDown(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
-				ENGINE_API static bool IsKeyPressed(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
-				ENGINE_API static bool IsKeyReleased(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
+				ENGINE_API virtual bool IsMouseButtonDown(int a_key) = 0;
+				ENGINE_API virtual bool IsMouseButtonPressed(int a_key) = 0;
+				ENGINE_API virtual bool IsMouseButtonReleased(int a_key) = 0;
 
-				ENGINE_API static bool IsMouseButtonDown(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
-				ENGINE_API static bool IsMouseButtonPressed(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
-				ENGINE_API static bool IsMouseButtonReleased(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
+				//ENGINE_API virtual bool IsMouseButtonReleased(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
+				//ENGINE_API virtual bool IsMouseButtonReleased(int a_key) { LOG_ERROR("Can't Cast! Default return value set to false!"); return false; }
 
 			protected:
 				static std::vector<int> currentKeyStatus;
