bd96956689d7b94e0adf98977cd106e837ba2a58
Corentin BOIS COTTE <c.boiscotte@student.isartdigital.com>
2025-05-08 00:19:41 +0200

    [UPDATE] Remove ugly sound and put windows xp instead

--- Editor/Code/src/Application.cpp
+++ Editor/Code/src/Application.cpp
@@ -61,7 +61,7 @@ namespace Editor
 			t_imguiLayer->AddWindow(new Ui::UiLoggerWindow(m_renderer, t_imguiLayer));
 			t_imguiLayer->AddWindow(new Ui::FileExplorerWindow(m_renderer, t_imguiLayer));
 
-			//Audio::SoundManager::Get().PlaySound("Assets/Sounds/fart.wav");
+			Audio::SoundManager::Get().PlaySound("Assets/Sounds/oxp.wav");
 		}
 
 		void Application::Run()
