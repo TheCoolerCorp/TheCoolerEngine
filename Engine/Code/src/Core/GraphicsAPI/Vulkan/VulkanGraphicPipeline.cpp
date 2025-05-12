ce7cf938c73b962ee6e73139cb0b92fa26eba877
e.rose <e.rose@student.isartdigital.com>
2025-05-12 09:50:17 +0200

    [FIX] Cheesy fix of skybox culling mode

--- Engine/Code/src/Core/GraphicsAPI/Vulkan/VulkanGraphicPipeline.cpp
+++ Engine/Code/src/Core/GraphicsAPI/Vulkan/VulkanGraphicPipeline.cpp
@@ -82,10 +82,19 @@ namespace Engine
 				rasterizer.rasterizerDiscardEnable = VK_FALSE;
 				rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
 				rasterizer.lineWidth = 1.0f;
-				rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+				//rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
 				// SWITCH HERE TO MAKE THE -X face appear
-				rasterizer.cullMode = VK_CULL_MODE_NONE;
-				//rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+
+				if (a_type == RHI::PipelineType::Other)
+				{
+					rasterizer.cullMode = VK_CULL_MODE_NONE;
+					rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+				}
+				else
+				{
+					rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+					rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
+				}
 				rasterizer.depthBiasEnable = VK_FALSE;
 				#pragma endregion
 
